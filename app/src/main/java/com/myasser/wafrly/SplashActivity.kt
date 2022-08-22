10ea6bf632918a7da038a9fbcf4cc0a7de0ebb5a
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-22 21:25:11 +0200

    Initial commit

---
+++ app/src/main/java/com/myasser/wafrly/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.myasser.wafrly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
+/**
+ * This project is a practice for:
+ *  1. APIs using Retrofit2
+ *  2. Coroutines
+ *  3. Remote storage using Firebase
+ *  4. Auth management using Firebase
+ *  5. Notifications using Firebase or custom notifications
+ *  6. MVVM architecture
+ */
