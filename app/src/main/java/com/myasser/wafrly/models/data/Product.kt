bf30ecc2181135c738c363b90043b75828bcfab1
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-27 01:29:35 +0200

    feat: added Product and Rating class for new API.

--- app/src/main/java/com/myasser/wafrly/models/data/Product.kt
+++ app/src/main/java/com/myasser/wafrly/models/data/Product.kt
@@ -1,5 +1,12 @@
 package com.myasser.wafrly.models.data
 
-class Product(){
-    //TODO: Add properties
-}
+data class Product(
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val description: String,
+    val Category: String,
+    val image: String,
+    val rating: Rating,
+)
+data class Rating(val rate: Double, val count: Int)
