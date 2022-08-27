3c106c1233aaa6df50dacb56b65d6680cc12393f
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-27 13:28:09 +0200

    feat: added initial functions (retrofit) for product. refactor: change functions' signature to suit new api. **Need to be tested.

--- app/src/main/java/com/myasser/wafrly/repository/IProductRepository.kt
+++ app/src/main/java/com/myasser/wafrly/repository/IProductRepository.kt
@@ -1,10 +1,9 @@
 package com.myasser.wafrly.repository
 
+import androidx.lifecycle.MutableLiveData
 import com.myasser.wafrly.models.data.Product
 
 interface IProductRepository {
-    fun getAllProducts(): List<Product>
-    fun getProductByName(name: String): List<Product> // may return list for same-name products
-    fun getTopRatedProducts(): List<Product>
-    fun getTopSalesProducts(): List<Product>
+    fun getAllProducts(): MutableLiveData<List<Product>>
+    fun getProductById(id: Int): MutableLiveData<Product> // may return list for same-name products
 }
--- app/src/main/java/com/myasser/wafrly/repository/ProductRepository.kt
+++ app/src/main/java/com/myasser/wafrly/repository/ProductRepository.kt
@@ -1,21 +1,54 @@
 package com.myasser.wafrly.repository
 
+import androidx.lifecycle.MutableLiveData
 import com.myasser.wafrly.models.data.Product
+import com.myasser.wafrly.models.database.RetrofitProductOperator
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class ProductRepository:IProductRepository {
-    override fun getAllProducts(): List<Product> {
-        TODO("Not yet implemented")
+// base url: https://fakestoreapi.com/products
+// product by id: baseurl/id
+// query: baseurl?sort=[desc | asc]& limit=[number]
+
+class ProductRepository : IProductRepository {
+    private val baseUrl = "https://fakestoreapi.com/products"
+    private val retrofitBuilder: Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val productService: RetrofitProductOperator =
+        retrofitBuilder.create(RetrofitProductOperator::class.java)
+
+    override fun getAllProducts(): MutableLiveData<List<Product>> {
+        val products = MutableLiveData<List<Product>>()
+        productService.getAllProducts().enqueue(object : retrofit2.Callback<List<Product>> {
+            override fun onResponse(
+                call: retrofit2.Call<List<Product>>,
+                response: retrofit2.Response<List<Product>>,
+            ) {
+                products.value = response.body()
+            }
+
+            override fun onFailure(call: retrofit2.Call<List<Product>>, t: Throwable) {
+                products.value = null
+            }
+        })
+        return products
     }
 
-    override fun getProductByName(name: String): List<Product> {
-        TODO("Not yet implemented")
-    }
+    override fun getProductById(id: Int): MutableLiveData<Product> {
+        val product = MutableLiveData<Product>()
+        productService.getProductById(id).enqueue(object : retrofit2.Callback<Product> {
+            override fun onResponse(
+                call: retrofit2.Call<Product>,
+                response: retrofit2.Response<Product>,
+            ) {
+                product.value = response.body()
+            }
 
-    override fun getTopRatedProducts(): List<Product> {
-        TODO("Not yet implemented")
-    }
-
-    override fun getTopSalesProducts(): List<Product> {
-        TODO("Not yet implemented")
+            override fun onFailure(call: retrofit2.Call<Product>, t: Throwable) {
+                product.value = null
+            }
+        })
+        return product
     }
 }
