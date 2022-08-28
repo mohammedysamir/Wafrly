57063af97b59ab41b0949c75bc12480365df6a5a
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-28 21:10:32 +0200

    feat: implemented category vm. feat: added GridView adapter for Categories. feat: created CategoryActivity.kt view.

--- app/src/main/java/com/myasser/wafrly/viewmodels/CategoryViewModel.kt
+++ app/src/main/java/com/myasser/wafrly/viewmodels/CategoryViewModel.kt
@@ -1,4 +1,11 @@
 package com.myasser.wafrly.viewmodels
 
+import androidx.lifecycle.LiveData
+import com.myasser.wafrly.models.data.Product
+import com.myasser.wafrly.repository.RetrofitCategoryRepository
+
 class CategoryViewModel {
+    val categoryRepo = RetrofitCategoryRepository()
+    fun getAllCategories(): LiveData<List<String>> = categoryRepo.getAllCategories()
+    fun getCategoryByName(name: String): LiveData<List<Product>> = categoryRepo.getCategoryByName(name)
 }
