e7737bb15c76e119941967e6326f25f4d4b9046c
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-28 23:28:50 +0200

    feat: added icons for filtering. feat: added function to filter products by price or name. feat: implement onClickListener in ProductsActivity.kt

--- app/src/main/java/com/myasser/wafrly/viewmodels/ProductsViewModel.kt
+++ app/src/main/java/com/myasser/wafrly/viewmodels/ProductsViewModel.kt
@@ -1,14 +1,28 @@
 package com.myasser.wafrly.viewmodels
 
+import com.myasser.wafrly.models.data.Product
 import com.myasser.wafrly.repository.RetrofitProductRepository
 
 class ProductsViewModel {
     private val productRepo = RetrofitProductRepository()
 
     fun getProducts() = productRepo.getAllProducts()
-    fun getProductById(id:Int)= productRepo.getProductById(id)
-    fun getProductsByCategory(category:String)= productRepo.getProductsByCategory(category)
+    fun getProductById(id: Int) = productRepo.getProductById(id)
+    fun getProductsByCategory(category: String) = productRepo.getProductsByCategory(category)
+
+    //todo: test sorting
+    fun getProductsSortedByPrice(category: String, isDescending: Boolean): List<Product> {
+        return if (!isDescending)
+            productRepo.getProductsByCategory(category).value?.sortedBy { it.price }?.reversed()!!
+        else
+            productRepo.getProductsByCategory(category).value?.sortedByDescending { it.price }!!
+    }
+
+    fun getProductsSortedByName(category: String, isDescending: Boolean): List<Product> {
+        return if (!isDescending)
+            productRepo.getProductsByCategory(category).value?.sortedBy { it.title }?.reversed()!!
+        else
+            productRepo.getProductsByCategory(category).value?.sortedByDescending { it.title }!!
+    }
 }
-//todo: add more filters by price (asc, desc) and by name (asc, desc)
 // product vm must be passed the product Id, send it to ProductRepo and get product back by id to view, and then navigate with intent extras
-//todo: here we must handle product item click -> navigate to ProductScreen and fetch data by id
--- app/src/main/java/com/myasser/wafrly/views/ProductsActivity.kt
+++ app/src/main/java/com/myasser/wafrly/views/ProductsActivity.kt
@@ -1,29 +1,86 @@
 package com.myasser.wafrly.views
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.color.MaterialColors
 import com.myasser.wafrly.R
 import com.myasser.wafrly.adapters.ProductRecyclerViewAdapter
-import com.myasser.wafrly.models.data.Product
 import com.myasser.wafrly.viewmodels.ProductsViewModel
 
-class ProductsActivity : AppCompatActivity() {
+class ProductsActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var productsRecyclerView: RecyclerView
     private val productsViewModel = ProductsViewModel()
+    private lateinit var adapter: ProductRecyclerViewAdapter
+    private val categoryName = intent.getStringExtra("category name")!!
+
+    companion object {
+        var sortedByPrice: Boolean = false
+        var sortedByName: Boolean = false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
-        val categoryName = intent.getStringExtra("category name")!!
         //set category name
         findViewById<TextView>(R.id.categoryTextView).text = categoryName
         //fetch products list using category name from the vm
         productsRecyclerView = findViewById(R.id.productsRecyclerView)
         //observe vm
         productsViewModel.getProductsByCategory(categoryName).observe(this) {
-            productsRecyclerView.adapter = ProductRecyclerViewAdapter(it, this)
+            adapter = ProductRecyclerViewAdapter(it, this)
+            productsRecyclerView.adapter = adapter
+        }
+    }
+
+    override fun onClick(v: View?) {
+        val onPrimaryColor =
+            MaterialColors.getColor(v?.context!!,
+                com.google.android.material.R.attr.colorOnPrimary,
+                Color.BLACK)
+        val whiteColor = Color.WHITE
+        when (v?.id) {
+            R.id.sortAlphabeticalImageView -> {
+                //re-fill recycler view with products sorted alphabetically
+                sortedByName = !sortedByName
+                adapter = ProductRecyclerViewAdapter(productsViewModel.getProductsSortedByName(
+                    categoryName,
+                    sortedByName), v.context)
+                productsRecyclerView.adapter = adapter
+                //change tint and background color
+                //todo: test color change of icon
+                val alphaIcon: ImageView = v as ImageView
+                if (sortedByName) {
+                    alphaIcon.setBackgroundColor(onPrimaryColor)
+                    alphaIcon.setColorFilter(whiteColor)
+                } else {
+                    alphaIcon.setBackgroundColor(Color.TRANSPARENT)
+                    alphaIcon.setColorFilter(onPrimaryColor)
+                }
+            }
+            R.id.sortPriceImageView -> {
+                //re-fill recycler view with products sorted by price
+                sortedByPrice = !sortedByPrice
+                adapter = ProductRecyclerViewAdapter(productsViewModel.getProductsSortedByPrice(
+                    categoryName,
+                    sortedByPrice), v.context)
+                productsRecyclerView.adapter = adapter
+
+                //todo: test color change of icon
+                val priceIcon: ImageView = v as ImageView
+                if (sortedByPrice) {
+                    priceIcon.setBackgroundColor(onPrimaryColor)
+                    priceIcon.setColorFilter(whiteColor)
+                } else {
+                    priceIcon.setBackgroundColor(Color.TRANSPARENT)
+                    priceIcon.setColorFilter(onPrimaryColor)
+                }
+            }
         }
     }
 }
+//when sorting is clicked .. change background color and tint color of the icon
