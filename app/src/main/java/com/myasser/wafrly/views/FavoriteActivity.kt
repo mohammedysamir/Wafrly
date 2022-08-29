e2292566adf9cb6957a65042715338540770b291
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-29 12:20:27 +0200

    feat: designed FavoriteActivity.kt. feat: delegate vm and hooked it with FavoriteActivity.kt view.

--- app/src/main/java/com/myasser/wafrly/views/FavoriteActivity.kt
+++ app/src/main/java/com/myasser/wafrly/views/FavoriteActivity.kt
@@ -1,12 +1,21 @@
 package com.myasser.wafrly.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.myasser.wafrly.R
+import com.myasser.wafrly.adapters.ProductRecyclerViewAdapter
+import com.myasser.wafrly.viewmodels.FavoriteViewModel
 
 class FavoriteActivity : AppCompatActivity() {
+    private val favoriteViewModel = FavoriteViewModel(this)
+    private lateinit var favoriteRecyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite)
+        favoriteRecyclerView = findViewById(R.id.favoriteRecyclerView)
+        favoriteViewModel.getFavorite().observe(this) {
+            favoriteRecyclerView.adapter = ProductRecyclerViewAdapter(it, this)
+        }
     }
 }
