ccaf7fe8c5f7dc6d2d44156566a764db123ef5ae
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-29 12:12:15 +0200

    feat: designed CartActivity.kt. feat: delegate VM to AccountRepository.kt feat: hook the view with the VM.

--- app/src/main/java/com/myasser/wafrly/viewmodels/CartViewModel.kt
+++ app/src/main/java/com/myasser/wafrly/viewmodels/CartViewModel.kt
@@ -1,4 +1,9 @@
 package com.myasser.wafrly.viewmodels
 
-class CartViewModel {
+import android.content.Context
+import com.myasser.wafrly.repository.AccountRepository
+
+class CartViewModel(private val context: Context) {
+    private val accountRepo=AccountRepository(context = context)
+    fun getCartItems()=accountRepo.getCart()
 }
--- app/src/main/java/com/myasser/wafrly/views/CartActivity.kt
+++ app/src/main/java/com/myasser/wafrly/views/CartActivity.kt
@@ -1,12 +1,22 @@
 package com.myasser.wafrly.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.myasser.wafrly.R
+import com.myasser.wafrly.adapters.ProductRecyclerViewAdapter
+import com.myasser.wafrly.viewmodels.CartViewModel
 
 class CartActivity : AppCompatActivity() {
+    private lateinit var cartRecyclerView: RecyclerView
+    private lateinit var cartViewModel: CartViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+        cartRecyclerView = findViewById(R.id.cartRecyclerView)
+        cartViewModel = CartViewModel(this)
+        cartViewModel.getCartItems().observe(this) {
+            cartRecyclerView.adapter = ProductRecyclerViewAdapter(it, this)
+        }
     }
 }
