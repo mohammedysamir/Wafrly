bab82df832c2e95562053408037862f53494cc6a
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-30 18:58:09 +0200

    refactor: change googleLogin() return type to GoogleSignInClient.

--- app/src/main/java/com/myasser/wafrly/models/database/AccountOperators.kt
+++ app/src/main/java/com/myasser/wafrly/models/database/AccountOperators.kt
@@ -1,11 +1,13 @@
 package com.myasser.wafrly.models.database
 
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.myasser.wafrly.models.data.Product
 
 interface AccountOperators {
     fun login(email: String, password: String): Boolean
-    fun googleLogin(): Boolean
+    fun googleLogin(): GoogleSignInClient
     fun register(email: String, password: String): Boolean
     fun addToCart(product: Product)
     fun addToFavorite(product: Product)
